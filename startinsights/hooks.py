a30c88d91dedc909c08f38e413552c79aacdd946
jagadeesan1104 <jagadeesan1104@gmail.com>
2024-02-09 11:18:03 +0530

    feat:service listing new args added

--- startinsights/hooks.py
+++ startinsights/hooks.py
@@ -28,7 +28,10 @@ app_license = "mit"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {"User" : "public/js/user.js"}
+doctype_js = {
+    "User" : "public/js/user.js",
+    "Course Lesson": "public/js/course_lesson.js",
+    }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
@@ -114,9 +117,9 @@ doctype_js = {"User" : "public/js/user.js"}
 # ---------------
 # Override standard doctype classes
 
-# override_doctype_class = {
-#	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+override_doctype_class = {
+	"Course Lesson": "startinsights.override.course_lesson.CustomCourseLesson"
+}
 
 # Document Events
 # ---------------
