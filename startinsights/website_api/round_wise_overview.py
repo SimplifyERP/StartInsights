93c1e7d88d92e4a63a6eaf001e4aa040b12cac52
jagadeesan1104 <jagadeesan1104@gmail.com>
2024-02-23 11:31:38 +0530

    feat:investor and round wise api changes

--- startinsights/website_api/round_wise_overview.py
+++ startinsights/website_api/round_wise_overview.py
@@ -26,7 +26,7 @@ def round_wise_form_title():
 
 # after showing the title form name enter the values and create a new rounded wise overview document.
 @frappe.whitelist()
-def create_roundwise_overview_list(round_name,round_type,closing_date_of_the_round,description,select_security_prefix,
+def create_round_wise_overview(round_name,round_type,closing_date_of_the_round,description,select_security_prefix,
                                 amount_raised,price_per_share,pre_money_valuation,dilution_for_this_round,user_id):
     try:
         closing_date = datetime.strptime(str(closing_date_of_the_round), "%d-%m-%Y").date()
@@ -45,6 +45,8 @@ def create_roundwise_overview_list(round_name,round_type,closing_date_of_the_rou
         round.color_code = color_code
         round.save(ignore_permissions=True)
         frappe.db.commit()
+
+        frappe.db.set_value('Round-wise Overview',round.name,'owner',user_id)
         message = " successfully Created Round-wise Overview"
         return {"status": True, "message": message}
     except Exception as e:
@@ -58,33 +60,22 @@ def get_random_color_code():
 
 #after created the rounded wise form to show the list view
 @frappe.whitelist()
-def get_round_wise_list_view():
+def get_round_wise_list_view(user_id):
     try:
-        get_round_wise = frappe.db.get_all("Round-wise Overview",['*']) 
+        get_round_wise = frappe.db.get_all("Round-wise Overview",{'creation_person_id':user_id},['*']) 
         formatted_round_wise= [] 
         for round_wise in get_round_wise:
-            description = html2text.html2text(round_wise.description).strip()
             round_wise = {
-                "title_name":"Name of the round",
-                "value_name":round_wise.name_of_the_round,
-                "title_round_type":"Round Type",
-                "value_round":round_wise.round_type,
-                "title_date":"Closing date of the Round",
-                "value_date":round_wise.closing_date_of_the_round,
-                "title_description":"Description",
-                "value_description":description,
-                "title_security":"Select Security Prefix",
-                "value_security":round_wise.select_security_prefix,
-                "title_amount":"Amount Raised",
-                "value_amount":round_wise.amount_raised,
-                "title_price":"Price per share",
-                "value_price":round_wise.price_per_share,
-                "title_pre_money":"Pre-Money Valuation",
-                "value_pre_money":round_wise.pre_money_valuation,
-                "title_dilution":"Dilution for this round (%)",
-                "value_dilution":round_wise.dilution_for_this_round,
-                "title_user":"Creation Person ID",
-                "value_user":round_wise.creation_person_id   
+                "name_of_the_round":round_wise.name_of_the_round,
+                "round_type":round_wise.round_type,
+                "closing_date_of_the_round":round_wise.closing_date_of_the_round,
+                "description":round_wise.description,
+                "select_security_prefix":round_wise.select_security_prefix,
+                "amount_raised":round_wise.amount_raised,
+                "price_per_share":round_wise.price_per_share,
+                "pre_money_valuation":round_wise.pre_money_valuation,
+                "dilution_for_this_round":round_wise.dilution_for_this_round,
+                "creation_person_id":round_wise.creation_person_id
             }
             formatted_round_wise.append(round_wise)
         return {"status":True,"round_wise":formatted_round_wise}
@@ -93,17 +84,14 @@ def get_round_wise_list_view():
 
 #the below method to show the graph percentage and color code
 @frappe.whitelist()
-def get_round_wise_graph():
+def get_round_wise_graph(user_id):
     try:
-        get_round_wise_data = frappe.db.get_all("Round-wise Overview",['name_of_the_round','dilution_for_this_round','color_code'])
-        formatted_round_wise_graph = []  
-        for round_wise in get_round_wise_data:
-            round_wise_graph = {
-                "name":round_wise.name_of_the_round,
-                "percentage":round_wise.dilution_for_this_round,
-                "color_code":round_wise.color_code
-            }
-            formatted_round_wise_graph.append(round_wise_graph)
-        return {"status":True,"round_wise_graph":formatted_round_wise_graph}
+        get_round_wise_data = frappe.db.get_value("Round-wise Overview",{'creation_person_id':user_id},['name_of_the_round','dilution_for_this_round','color_code'])
+        round_wise_graph = {
+            "name":get_round_wise_data[0],
+            "percentage":get_round_wise_data[1],
+            "color_code":get_round_wise_data[2]
+        }
+        return {"status":True,"round_wise_graph":round_wise_graph}
     except Exception as e:
         return {"status":False,"message":e}
