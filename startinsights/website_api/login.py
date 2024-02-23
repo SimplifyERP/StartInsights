34e81687956f9c0d1177cb5043528d96c497ef8f
janishatech <janisha1508@gmail.com>
2024-02-23 14:09:29 +0530

    feature:login role added

--- startinsights/website_api/login.py
+++ startinsights/website_api/login.py
@@ -1,32 +1,36 @@
+
 import frappe
-from frappe.utils.password import check_password,get_decrypted_password
-from frappe.utils import  get_url
-
-
-
+from frappe.utils.password import check_password, get_decrypted_password
+from frappe.utils import get_url
 @frappe.whitelist(allow_guest=True)
-def user_login(username,password):
+def user_login(username, password):
     status = ""
     message = ""
     image_url = ""
+    role = ""
     try:
         user = frappe.get_doc("User", username)
-        if user and check_password(user.name,password):
+        login_type_doc = frappe.get_all("Login Type", filters={"user": user.name}, fields=["user_type"])
+        if login_type_doc:
+            role = login_type_doc[0].user_type
+        if user and check_password(user.name, password):
             if user.cover_image:
                 image_url = get_url() + user.cover_image
             else:
                 image_url = ""    
-            user_deatils = {
-                "user_name" : username,
-                "full_name":user.full_name,
-                "phone_no":user.phone or "",
-                "user_email":user.email,
-                "profile_image":image_url
+
+            user_details = {
+                "user_name": username,
+                "full_name": user.full_name,
+                "phone_no": user.phone or "",
+                "user_email": user.email,
+                "role": role ,
+                "profile_image": image_url
             }
             status = True
-            message = "Login Successfull"
-            return {"status":status,"message":message,"userinfo":user_deatils}
+            message = "Login Successful"
+            return {"status": status, "message": message, "userinfo": user_details}
     except Exception as e:
         status = False
-        message = e
-        return {"status":status,"message":message}    
+        message = str(e)
+        return {"status": status, "message": message}    
