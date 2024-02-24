5e0b2b5ba4a20f8eba6493ee38c6b96a08b7d8df
jagadeesan1104 <jagadeesan1104@gmail.com>
2024-02-24 17:14:23 +0530

    feat:profile and login changes in api

--- startinsights/website_api/login.py
+++ startinsights/website_api/login.py
@@ -1,35 +1,42 @@
-
 import frappe
 from frappe.utils.password import check_password, get_decrypted_password
 from frappe.utils import get_url
+
+    
 @frappe.whitelist(allow_guest=True)
 def user_login(username, password):
     status = ""
     message = ""
     image_url = ""
-    role = ""
     try:
         user = frappe.get_doc("User", username)
-        login_type_doc = frappe.get_all("Login Type", filters={"user": user.name}, fields=["user_type"])
-        if login_type_doc:
-            role = login_type_doc[0].user_type
-        if user and check_password(user.name, password):
-            if user.cover_image:
-                image_url = get_url() + user.cover_image
-            else:
-                image_url = ""    
+        if user.name:
+            if user and check_password(user.name, password):
+                profile = frappe.get_doc("Profile Application",username)
+                if profile.profile_image:
+                    image_url = get_url() + profile.profile_image
+                else:
+                    image_url = ""    
 
-            user_details = {
-                "user_name": username,
-                "full_name": user.full_name,
-                "phone_no": user.phone or "",
-                "user_email": user.email,
-                "role": role ,
-                "profile_image": image_url
-            }
-            status = True
-            message = "Login Successful"
-            return {"status": status, "message": message, "userinfo": user_details}
+                user_details = {
+                    "user_name": profile.user_id,
+                    "full_name": profile.full_name,
+                    "user_email": profile.email_id,
+                    "company_name":profile.company_name,
+                    "phone_no": profile.phone_no,
+                    "designation":profile.designation or "",
+                    "linkedin":profile.linkedin,
+                    "profile_image":image_url or "",
+                    "login_type":profile.login_type or "",
+                    "role": profile.customer_group ,
+                }
+            else:
+                message = "please contact support team" 
+        else:
+            message = "please contact support team"        
+        status = True
+        message = "Login Successful"
+        return {"status": status, "message": message, "userinfo": user_details}
     except Exception as e:
         status = False
         message = str(e)
