c3ec8c4f70d2d559ad9b5180167d454b42f965dd
janishatech <janisha1508@gmail.com>
2024-03-09 14:31:53 +0530

    feature: investor application email send

--- startinsights/start_insights/doctype/investor_application/investor_application.js
+++ startinsights/start_insights/doctype/investor_application/investor_application.js
@@ -1,12 +1,27 @@
 // Copyright (c) 2024, Suriya and contributors
 // For license information, please see license.txt
 
+
 frappe.ui.form.on("Investor Application", {
-	refresh:function(frm){
-        if (frm.doc.docstatus==0){
-            frm.add_custom_button(__('Sent Mail Verify Code'), function (){
-            }).css('background-color', '#0EE30E');	
+    refresh: function(frm) {
+        if (frm.doc.docstatus == 0) {
+            frm.add_custom_button(__('Send Verification Code Email'), function() {
+                if (!frm.doc.verify_code_email_sent) {
+                    frappe.call({
+                        method: 'startinsights.start_insights.doctype.investor_application.investor_application.send_verification_email',
+                        args: {
+                            name: frm.doc.name,
+                            email_id :frm.doc.email_id
+                        },
+                        callback: function(r) {
+                            if (r.message) {
+                            }
+                        }
+                    });
+                } else {
+                    frappe.msgprint('Verification code email has already been sent.');
+                }
+            }).css('background-color', '#0EE30E');
         }
-			
-	},
+    }
 });
