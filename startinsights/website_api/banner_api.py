3fa2bef02ab2bfcd426e5b706b1928b17d2a6aca
jagatechsavant <jagadeesan.a1104@gmail.com>
2024-04-02 13:34:24 +0530

    feat:fundraising experts changes in service listing

--- startinsights/website_api/banner_api.py
+++ startinsights/website_api/banner_api.py
@@ -4,11 +4,28 @@ from startinsights.custom import get_domain_name
 
 @frappe.whitelist()
 def get_banner(type_of_user):
+    banner_image_url = ""
     try:
-        get_banner_data = frappe.db.get_all("Dashboard Banner",{"disabled":1},['*'])
+        get_banner_data = frappe.db.get_all("DashBoard Banner",{"customer_group":type_of_user},['*'])
+        banner_list = []
         for banner in get_banner_data:
-            if banner.customer_group == type_of_user:
-                pass
-        return {"status":True}
+            if banner.banner_image:
+                banner_image_url = get_domain_name() + banner.default_banner_image
+            else:
+                banner_image_url = ""    
+            banner_details = {
+                "id":banner.name,
+                "name":banner.name,
+                "title":banner.banner_name or "",
+                "description":banner.description or "   ",
+                "banner_type":banner.type_of_banner or "",
+                "banner_image":banner_image_url,
+                "type_of_user":banner.customer_group or "",
+                "ad_start_date":banner.ad_start_date_time or "",
+                "ad_end__time":banner.ad_end_date_time or ""
+            }
+            banner_list.append(banner_details)
+                
+        return {"status":True,"banner":banner_list}
     except Exception as e:
         return {"status":False,"message":e}
--- startinsights/website_api/service_list.py
+++ startinsights/website_api/service_list.py
@@ -37,13 +37,13 @@ def create_service_list(expert_name,service_date,start_time,end_time,user,bookin
     
 # mark the booked status in child table
 def mark_booked_status(booking_id,start_time,end_time,date):
-    get_book_an_expert = frappe.get_doc("Book an Expert", booking_id)
-    get_booking_table = frappe.db.get_all("Book an Expert Table",filters={'parent': get_book_an_expert.name},fields=['*'],order_by='idx ASC')
+    get_book_an_expert = frappe.get_doc("Fundraising Experts", booking_id)
+    get_booking_table = frappe.db.get_all("Fundraising Expert Table",filters={'parent': get_book_an_expert.name},fields=['*'],order_by='idx ASC')
     for entry in get_booking_table:
         if entry.date == date:
             if get_time(entry.start_time) == get_time(start_time) and get_time(entry.end_time) == get_time(end_time):
-                frappe.db.set_value("Book an Expert Table",entry.name,'booked_status','1')
-                frappe.db.set_value("Book an Expert Table",entry.name,'status','True')
+                frappe.db.set_value("Fundraising Expert Table",entry.name,'booked_status','1')
+                frappe.db.set_value("Fundraising Expert Table",entry.name,'status','True')
 
 
 @frappe.whitelist()
