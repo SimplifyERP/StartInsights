e615f2be19a92ee84a38ee04176dd9e832a01721
jagadeesan.a1104@gmail.com <jagadeesan.a1104@gmail.com>
2024-05-04 18:34:15 +0530

    feat:fundability quiz doctypes

--- startinsights/start_insights/doctype/service_payment/service_payment.py
+++ startinsights/start_insights/doctype/service_payment/service_payment.py
@@ -7,20 +7,24 @@ from frappe.model.document import Document
 
 class ServicePayment(Document):
     pass
+    
 
     
-    # def validate(self):
-    #     frappe.errprint("hi")
-    #     frappe.errprint(self.create_sales_invoice())
-
-    # def create_sales_invoice(self):
-    #     new_sales_invoice = frappe.new_doc("Sales Invoice")
-    #     new_sales_invoice.customer = self.get_customer()
-    #     new_sales_invoice.due_date = self.service_booked_date
-    #     new_sales_invoice.save(ignore_permissions=True)
-    #     frappe.db.commit()
+    def after_submit(self):
+        new_sales_invoice = frappe.new_doc("Sales Invoice")
+        new_sales_invoice.customer = self.get_customer()
+        new_sales_invoice.due_date = self.service_booked_date
+        new_sales_invoice.append("items",{
+            "item_code":self.service_id,
+            "qty":1,
+            "uom":"Nos",
+            "rate":self.amount,
+            "amount":self.amount,
+        })
+        new_sales_invoice.save(ignore_permissions=True)
+        frappe.db.commit()
     
-    # def get_customer(self):
-    #     customer = frappe.db.get_value("Customer",{"custom_user_name":self.login_user},["name"])
-    #     return customer
+    def get_customer(self):
+        customer = frappe.db.get_value("Customer",{"custom_user_name":self.login_user},["name"])
+        return customer
     
